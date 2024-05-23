ff1afc6e4378aa6f5c20ef574e03665cffa07c7b
yasamin daei <yasamindaei8@gmail.com>
2024-05-23 14:26:00 +0330

    adding class user

--- models.py
+++ models.py
@@ -15,3 +15,20 @@ class Priority(Enum):
     DOING = 3
     DONE = 4
     ARCHIVED = 5
+
+    class User:
+    users = []
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.is_active = True
+        User.users.append(self)
+
+    @classmethod
+    def find_user_by_username(cls, username):
+        for user in cls.users:
+            if user.username == username:
+                return user
+        return None
